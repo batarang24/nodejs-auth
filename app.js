8dfdd9bfefb87449de6a1e83aa263d595a544e1f
batarang37 <76785483+batarang37@users.noreply.github.com>
2024-03-02 18:50:05 +0530

    plays with cookie

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(express.json());
-
+//
 
 // database connection
 const uri='mongodb+srv://naren:hello123@cluster0.hedlmiz.mongodb.net/untitled';
@@ -15,3 +16,4 @@ mongoose.connect(uri,{})
 // routes
 app.get('/', (req, res) => res.send('Hello world'));
 app.use(router)
+app.use(cookieParser);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,15 @@
 const User = require("../models/User");
+const jwt=require("jsonwebtoken");
 
 module.exports.signup_get=(req,res)=>{
     res.send('signup get')
 }
-
+const maxage=3*24*60*60;
+const createjwt=(id)=>{
+  return jwt.sign({id},"helloworld",{
+    "expiresIn":maxage
+  })
+}
 const errorsol=(err)=>{
   var errors={
     email:'',
@@ -29,7 +35,10 @@ module.exports.signup_post=async(req,res)=>{
 
     try {
       const user = await User.create({ email, password });
-      res.status(201).json(user);
+      const jwt=createjwt(user._id);
+    
+      res.cookie('jwt',jwt,{httpOnly:true,maxAge:maxage*1000})
+      res.status(201).json(user._id);
     }
     catch(err) {
       console.log(err)
